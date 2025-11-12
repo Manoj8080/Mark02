1219181251c12ce354d98a6997a5b9ae8d23ddcc
Manoj8080 <uisloading@gmail.com>
2025-11-12 10:45:00 +0530

    sample

--- app/src/main/java/com/nukkadshops/mark02/PayOptions.java
+++ app/src/main/java/com/nukkadshops/mark02/PayOptions.java
@@ -1,6 +1,7 @@
 package com.nukkadshops.mark02;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class PayOptions extends AppCompatActivity {
 
+    Button card;
+    Button upi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pay_options);
+
+        card = findViewById(R.id.cardbt);
+        upi = findViewById(R.id.upibt);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/nukkadshops/mark02/Processing.java
+++ app/src/main/java/com/nukkadshops/mark02/Processing.java
@@ -1,9 +1,6 @@
 package com.nukkadshops.mark02;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,21 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    //TextView textView;
-    EditText editText;
-    Button ok;
+public class Processing extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        editText = findViewById(R.id.amount);
-        ok = findViewById(R.id.proceed);
-
+        setContentView(R.layout.activity_processing);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
