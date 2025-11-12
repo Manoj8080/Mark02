eb52316bbce94a92d5a19911c3452053116cef3b
Manoj8080 <uisloading@gmail.com>
2025-11-12 16:26:36 +0530

    sample

--- app/src/main/java/com/nukkadshops/mark02/MainActivity.java
+++ app/src/main/java/com/nukkadshops/mark02/MainActivity.java
@@ -1,10 +1,13 @@
 package com.nukkadshops.mark02;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -33,5 +36,60 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+
+        card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /*here api calling
+                        .
+                .
+                .
+                */
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Payment Processing..!")
+                        .setCancelable(false) // prevents closing by tapping outside
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss(); // closes the dialog
+                                // You can also handle cancel logic here if needed
+                            }
+                        });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+        upi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /*write api code here
+                .
+                .
+                .
+                .*/
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Payment Processing..!")
+                        .setCancelable(false) // prevents closing by tapping outside
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss(); // closes the dialog
+                                // You can also handle cancel logic here if needed
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+
+            }
+        });
+
     }
 }
